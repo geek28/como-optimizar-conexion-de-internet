de911d96837ca10593ebff165dfda378563bd6ad
geek28 <cubanazo40@yahoo.es>
2025-05-27 16:47:18 -0400

    network speed up

---
+++ network speed up.bat
@@ -0,0 +1,9 @@
+ipconfig /release  
+ipconfig /renew 
+ipconfig /flushdns  
+netsh int tcp set global chimney=enabled
+netsh int tcp set global autotuninglevel=normal
+netsh int tcp set global dca=enabled  
+netsh int tcp set global netdma=enabled
+netsh int tcp set global ecncapability=enabled
+netsh int tcp set global congestionprovider=ctcp
